d878f8247ed8802957f959e6f1741d7ed702a26e
alan916 <61741669+alan916@users.noreply.github.com>
2021-06-09 11:49:27 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -38,9 +38,15 @@
 <th>Editar</th>
 <th>Eliminar</th>
 </tr>
+<?php foreach($datos in $dato): ?>
 <tr>
-<?php foreach
+<th><?php echo $dato->nombre ?></th>
+<th><?php echo $dato->apellido_paterno ?></th>
+<th><?php echo $dato->apellido materno ?></th>
+<th> <a href="<?php base_url('/index.php/editar') ?>" class="btn btn-sm btn-warning">Editar</a> </th>
+<th> <a href="<?php_base_url('/index.php/eliminar') ?>" class="btn btn-sm btn-danger">Editar</a></th>
 </tr>
+<?php endforeach ?>
 </table>
 </div>
 </div>
