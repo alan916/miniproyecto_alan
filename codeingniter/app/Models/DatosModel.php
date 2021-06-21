175089ae7a19bbcd44d164ef87e46a0ff6f9eeec
alan916 <61741669+alan916@users.noreply.github.com>
2021-06-21 14:44:33 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -1,10 +1,30 @@
 <?php
 namespace App\Models;
 use CodeIgniter\Model;
-
-class DatosModel extends Model{
-public function listar(){
-$m = $this->db->query("SELECT * from persona");
-return $m->getResult();
+class GeneralModel extends Model{
+public function obtenerInformacion ($data){
+$gModel $this->db->table('persona');
+$gModel->where($data);
+return $gModel->get()->getResultArray();
+}
+public function listarTodo(){
+$gModel $this->db->query("SELECT * FROM persona");
+return $gModel->getResult();
+}
+public function insertar($data){
+$gModel = $this->db->table('persona');
+$gModel->insert ($data);
+return $this->db->insertID();
+}
+public function actualizar($data,$id){
+$gModel $this->db->table('persona');
+$gModel->set($data);
+$gModel->where($id);
+return $gModel->update();
+}
+public function eliminar($id){
+$gModel = $this->db->table('persona');
+$gModel->where($id);
+return $gModel->delete();
 }
 }
